248031396c674af819580f93cef8492d1b632d64
Lavanya K <lavanya.k@robosoftin.com>
2024-01-02 22:46:32 +0530

    Init

---
+++ src/main/java/com/lavi/sprint/boot/with/react/SprintBootWithReactApplication.java
@@ -0,0 +1,13 @@
+package com.lavi.sprint.boot.with.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootWithReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootWithReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lavi/sprint/boot/with/react/SprintBootWithReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavi.sprint.boot.with.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootWithReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
